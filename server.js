9c60880a45859c77c98a887fd9ab976ca6fb717c
Adriano <56710943+ASRosa@users.noreply.github.com>
2020-03-26 11:54:52 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).send({ message: 'Hello WORUDO!' });
+    res.status(200).send({ message: 'Hello WORLDO!' });
 })
 
 app.get('/ola', (req, res) => {
-    res.status(200).send({ message: 'Hello WORUDO!' });
+    res.status(200).send({ message: 'rota 1' });
 })
 
 app.listen(3001, () => {
